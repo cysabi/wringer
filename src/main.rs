bd02b1e1653f92ae2ba4a4b2b6ee854952a33f71
Miles Wirht <regime_skylark.0y@icloud.com>
2025-06-16 23:22:06 -0400

    IMPORTS

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
-use webview2_com::Microsoft::Web::WebView2::Win32;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::{
+    path::Path,
+    sync::{Arc, Mutex, mpsc},
+    thread,
+    time::Duration,
+};
+
+use dpi::{PhysicalPosition, Size};
+use ndarray::Array3;
+use tracing_subscriber;
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowId},
+};
+use wry::Rect;
+use wry::WebViewBuilder;
+use wry::WebViewExtMacOS;
+
+use video_rs::encode::{Encoder, Settings};
+use video_rs::init as video_init;
+use video_rs::time::Time;
 
 fn main() {
     // make a webview
